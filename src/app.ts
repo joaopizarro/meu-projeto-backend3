43ff5bd1e66d30147d2d8ca106543d360935ec3f
joaopizarro <joaopizarro0@gmail.com>
2026-09-22 11:04:59 -0300

    meu_projeto_backend3

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
 import express from "express";
 import type { Express, Request, Response } from "express";
 
+import fs from "fs";
 // Importa a classe Player
 import { Player } from "./models/player.js";
 
@@ -14,9 +15,55 @@ app.use(express.json());
 // Define a porta do servidor
 const PORT: number = 8081;
 
-// Cria um novo jogador chamado "Hero"
-// Nome: Hero | Vida: 100 | Nível: 5
-const player1: Player = new Player("Hero", 100, 5);
+// Define o nome do diretório onde os arquivos serão armazenados
+const DATA_FILE = "./data/players.json";
+
+/*
+Função para garantir que o diretório de dados exista antes de salvar os arquivos.
+Se o diretório não existir, ele será criado
+*/
+function ensureDataFolderExists() {
+    const dataFolder = "./data";
+    if (!fs.existsSync(dataFolder)) {
+        fs.mkdirSync(dataFolder);
+    }
+}
+
+// Chamar a função para garantir que o diretório de dados exista
+// antes de qualquer operação de leitura ou escrita de arquivos
+ensureDataFolderExists();
+
+// Função para salvar os dados do player em um arquivo JSON
+function savePlayerState(player: Player) {
+    // Converte o objeto player em uma string JSON
+    const data = JSON.stringify(player, null, 2);
+    // Salva a string JSON no arquivo definido em DATA_FILE
+    fs.writeFileSync(DATA_FILE, data, "utf-8");
+}
+
+// Função para carregar os dados do player de um arquivo JSON
+function loadPLayerState(): Player {
+    // Verifica se oo arquivo de dados existe
+    if (fs.existsSync(DATA_FILE)) { 
+    // Lê o conteúdo do arquivo e converte de volta para um objeto Player
+    const data = fs.readFileSync(DATA_FILE, "utf-8");
+    const playerData = JSON.parse(data);
+
+    /* ATENÇÃO: JSON.parse() retorna um objeto "puro" (sem os métodos da classe Player)
+    Para que o objeto tenha os métodos da classe Player, precisamos criar uma nova instância da
+    classe Player e passar os dados carregados para o construtor.
+    */
+    return new Player(playerData.name, playerData.health, playerData.level);
+    }
+    // Cria um novo jogador se não existir chamado "Hero"
+    // Nome: Hero | Vida: 100 | Nível: 5
+    const newPlayer: Player = new Player("Hero", 100, 5);
+    savePlayerState(newPlayer);
+    return newPlayer;
+}
+// Inicializa o player carregando seu estado do arquivo JSON
+let player1: Player = loadPLayerState();
+
 
 // GET
 // Quando o usuário acessa /player,
@@ -44,7 +91,8 @@ app.post("/player/damage", (req: Request, res: Response) => {
     const { damage } = req.body;
 
     const damageMessage = player1.takeDamage(damage);
-
+    // Salvar o estado atual do player no arquivo JSON
+    savePlayerState(player1);
     res.json({
         action: damageMessage,
         currentHealth: player1.health,
